f1b7790e48c8c24548d2972dbaaf61460b21eaf8
Adam M. Wilson <adamw@buffalo.edu>
2021-09-15 20:10:36 -0400

    Update test-CS02.R

--- test-CS02.R
+++ test-CS02.R
@@ -7,15 +7,17 @@ library(testthat)
 # Case study script
 file="week_02/case_study_02.R"
 
+context("Case Study 02 - File Exists")
 test_that(paste0(file, " file exists"),{
   expect_true(file.exists(file))
 })
 
+context("Case Study 01 - File Sources without errors")
 test_that(paste0(file, " file sources without errors"), {
 expect_silent(source(file))
 })
 
-
+context("Case Study 01 - Results")
 test_that(paste0(file, " file includes geom_smooth"), {
   expect_true(grepl("geom_smooth",scan(file)))
 })
