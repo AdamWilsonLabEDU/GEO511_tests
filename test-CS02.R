9f060fc2164ae9e716fe0d9977085a3cd54d3e5b
Adam M. Wilson <adamw@buffalo.edu>
2021-09-15 21:03:53 -0400

    Update test-CS02.R

--- test-CS02.R
+++ test-CS02.R
@@ -12,12 +12,12 @@ test_that(paste0(file, " file exists"),{
   expect_true(file.exists(file))
 })
 
-context("Case Study 01 - File Sources without errors")
+context("Case Study 02 - File Sources without errors")
 test_that(paste0(file, " file sources without errors"), {
 expect_silent(source(file))
 })
 
-context("Case Study 01 - Results")
+context("Case Study 02 - Results")
 test_that(paste0(file, " file includes geom_smooth"), {
   expect_true(any(grepl("geom_smooth",readLines(file))))
 })
